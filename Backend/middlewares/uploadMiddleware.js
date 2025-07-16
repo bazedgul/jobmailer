7fbca332ab9e6eef7431e2af2343837bf6970b9a
bazedgul <bazedmmii@gmail.com>
2025-07-16 21:22:17 +0500

    middleware logic fixed

--- Backend/middlewares/uploadMiddleware.js
+++ Backend/middlewares/uploadMiddleware.js
@@ -1,10 +1,16 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
+// Ensure uploads folder exists
+const uploadDir = path.join("uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 // Storage config
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/"); // Save in uploads/ directory
+    cb(null, uploadDir); // Save in uploads/ directory
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
